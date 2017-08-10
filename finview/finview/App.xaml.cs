a6d7d9fd581df02d94d7d25bd9ac83ba70279384
Nivash <nivash@outlook.com>
2017-08-11 00:37:11 +0530

    Error handling, migration helper, script

--- finview/finview/App.xaml.cs
+++ finview/finview/App.xaml.cs
@@ -17,6 +17,19 @@ namespace finview
     /// </summary>
     public partial class App : Application
     {
+		
+		public App():base() 
+		{
+			this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+		}
+
+		private void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) 
+		{
+			string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+			MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			e.Handled = true;
+		}
+	
         protected override void OnStartup(StartupEventArgs e)
         {
 
